96388a9d2e9c21575157ebaccab30143ef967822
miae094 <miae094@gmail.com>
2025-02-17 20:09:57 +0900

    배열문제풀이

--- 04_array/src/edu/kh/array/ex/ArrayExample1.java
+++ 04_array/src/edu/kh/array/ex/ArrayExample1.java
@@ -147,6 +147,274 @@ public class ArrayExample1 {
 		System.out.printf("평균 : %.2fcm",sum/(height.length) );
 	}
 	
+	public void ex4() {
+		// 입력 받은 인원 수 만큼의 점수를 입력받아 배열에 저장
+		// 입력이 완료되면 점수, 합계, 평균, 최고점, 최저점 출력
+		
+		// ex)
+		// 입력 받을 인원 수 : 4
+		// 1번 점수 입력 : 100
+		// 2번 점수 입력 : 80
+		// 3번 점수 입력 : 50
+		// 4번 점수 입력 : 60
+		
+		// 합계 : 290
+		// 평균 : 72.5
+		// 최고점 : 100
+		// 최저짐 : 50
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("입력 받을 인원 수 : ");
+		int input = sc.nextInt();
+		
+		// 배열 선언 및 할당
+		// 할당할 배열의 크기는 입력받은 
+		int[] score = new int[input];
+		
+		// 합계 저장용 변수
+		int sum = 0;
+		
+		// score 배열의 각 요소에 입력하는값 대입 & 합계 누적 용도
+		for(int i = 0; i < score.length; i++) {
+			System.out.print( (i + 1) + "번 점수 입력 : "); 
+			score[i] = sc.nextInt();
+			// score[0] = 100
+			// score[1] = 50
+			// score[2] = 30
+			// score[3] = 90
+			sum += score[i];
+			
+		}
+		
+		// ===============================
+		// -> score 배열의 각 요소가 모두 입력한 값으로 대입 완료
+		
+		// 최저점과 최고점의 기준 설정
+		int max = score[0];
+		int min = score[0];
+		
+		for(int i = 0; i < score.length; i++) {
+			// 0 1 2 3 
+			
+			// 최고점 비교
+			if( score[i] > max ) {
+				max = score[i];
+			}
+			
+			// 최저점 비교
+			if( score[i] < min ) {
+				min = score[i];
+			}
+			
+		}
+		
+		System.out.println("합계 : " + sum);
+		System.out.printf("평균 : %.2f\n", (double)sum / score.length );
+		System.out.println("최고점 : " + max);
+		System.out.println("최저점 : " + min);
+		
+	}
 	
+	public void ex5() {
+		// 점심 메뉴 뽑기 프로그램
+		String[] arr = {"김밥", "서브웨이", "햄버거", "백반", "국밥", "짜장면"};
+		
+		System.out.println("오늘 점심 메뉴 : " + arr[ (int)(Math.random() * 6) ]);
+		// (int)(Math.random() * 6) 
+		// Math.random() : 0.0 ~ 1.0
+		// 0 1 2 3 4 5
+		// 0.0 <= x < 1.0
+		// 0.0 <= x * 6 < 6.0
+		// 0 <= x < 6 
+		
+	}
+	
+	public void ex6() {
+		// 배열을 이용한 검색
+		
+		// 입력받은 정수가 배열에 있는지 없는지 확인
+		// 만약 있다면 몇번 인덱스에 존재하는지 출력
+		
+		// ex)
+		// 정수 입력 : 200
+		// 1번째 인덱스에 존재
+		
+		// 정수 입력 : 5
+		// 존재하지 않음
+		
+		int[] arr = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("정수 입력 : ");
+		int input = sc.nextInt();
+		
+		// 신호를 나타내기 위한 변수
+		boolean flag = false; // 검사전에는 없다고 가정
+		
+		for(int i = 0; i < arr.length; i++) {
+			
+			if(arr[i] == input) {
+				System.out.println(i + "번째 인덱스에 존재");
+				flag = true; // 일치하는 값이 있으므로 true로 변경
+			} 
+			
+		}
+		
+		// flag 상태를 검사
+		// flag가 false 라면 일치하는 값을 찾지 못했다라는 의미
+		// 그런데 flag가 false라면 if 조건식이 false이므로 수행되지 않음
+		// -> ! (Not 연산자 : 부정 논리 연산자) 붙여줌으로써 true 로 변경하여
+		// {} 안에있는 코드를 수행하게끔 함.
+		
+		if( !flag  ) {
+			System.out.println("존재하지 않음");
+		}
+		
+		
+		
+	}
+	
+	public void ex7() {
+		// 입력받은 값과 일치하는 값이 있으면 인덱스번호 출력
+		// 없으면 "존재하지 않음" 출력
+		
+		String[] arr = {"사과", "딸기", "바나나", "키위", "멜론", "아보카도"};
+		
+		/*
+		 * 과일 입력 : 아보카도
+		 * 5번 인덱스에 존재
+		 * 
+		 * 과일 입력 : 수박
+		 * 존재하지 않음
+		 * 
+		 * */
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("과일 입력 : ");
+		String input = sc.next();
+		
+		boolean flag = true;
+		
+		for(int i = 0; i < arr.length; i++) {
+			
+			if( arr[i].equals(input) ) { // String 비교시 equals() 사용!
+				System.out.println(i + "번째 인덱스에 존재");
+				flag = false;
+			}
+		}
+		
+		if( flag ) {
+			System.out.println("존재하지 않음");
+		}
+		
+		
+	}
+	
+	public void ex8() {
+		
+		// String 참조형 
+		
+		String str = "가나다"; // String Constant Pool 저장
+		String str2 = new String("가나다"); // Heap 새 객체 저장
+		
+		System.out.println(str); // 주소값?
+		System.out.println(str2);
+		System.out.println(str.toString());
+		
+		// String 클래스의 toString() 메서드가 존재
+		// toString() 메서드는 자기 자신을 그대로 반환.
+		
+		// System.out.println(str); 을 실행하면
+		// str.toString() 이 자동으로 호출됨
+		// 대입해둔 문자열 값 자체가 반환됨..
+		
+		// 주소값(객체의 해시코드)을 확인하고 싶다면 아래처럼 사용..
+		System.out.println(System.identityHashCode(str));
+		
+		String[] strArr = {"사과", "바나나", "딸기"};
+		// 메모리 구조가 어떻게 생겼을까? 
+		
+		
+		
+	}
+	
+	public void ex9() {
+		// 1. 문자열을 입력받아 한 글자씩 잘라내어 char 배열에 순서대로 저장
+		// 2. 문자 하나를 입력받아 일치하는 문자가 char 배열에 몇개 존재하는지 확인
+		// 3. 단, 일치하는 문자 없을경우 "존재하지 않습니다" 출력
+		
+		/*
+		 * ex) 
+		 * 
+		 * 문자열 입력 : hello
+		 * [h, e, l, l, o]
+		 * 검색할 문자 입력 : l
+		 * 2개 있음 / 존재하지 않습니다
+		 * 
+		 * */
+		
+		// 사용해야하는 기술, 기능
+		// 1) 배열 검색
+		// 2) 문자열의 길이 구하기 : String.length() 
+		//   ex ) "hello".length() -> 5  
+		
+		// 3) String.charAt(index) : 문자열에서 특정 index에 위치한 문자 하나를 얻어옴. 
+		// ex) "hello".charAt(1) -> 'e'
+		//      01234
+		
+		// 4) count (숫자세기)
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("문자열 입력 : ");
+		String input = sc.nextLine(); // hello world [h,e,l,l,o, ,w,o,r,l,d]
+		
+		// 1. 입력받은 문자열을 한글자씩 잘라내어 char 배열에 순서대로 저장
+		char[] arr = new char[input.length()];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = input.charAt(i); // hello
+			// arr[0] = 'h';
+			// arr[1] = 'e';
+			// arr[2] = 'l';
+			// arr[3] = 'l';
+			// arr[4] = 'o';
+		}
+		
+		System.out.println( Arrays.toString(arr) );
+		
+		// 2. 문자하나를 입력받아 일치하는 문자가 char 배열에 몇개 존재하는지 확인
+		System.out.print("검색할 문자 입력 : ");
+		char ch = sc.next().charAt(0);
+		// sc.next() -> String형
+		// String.chatAt(0) -> String의 0번째 인덱스 문자 하나를 추출해서 char 형태로 반환
+		// "h" -> 'h'
+		
+		int count = 0; // 같은 글자 개수를 세기 위한 변수
+		
+		//[h,e,l,l,o] -> 5
+		for(int i = 0; i < arr.length; i++) {
+			
+			if(arr[i] == ch) {
+				count++; 
+				// arr[i] 값과 검색할 문자 ch가 같은경우
+				// 카운트를 늘려라!
+			}
+			
+			
+		}
+		
+		// 결과 출력
+		if( count > 0 ) { // 일치하는 값 있음
+			System.out.println(count + "개 있음");
+			
+		} else { // 일치하는 문자가 없음
+			// 3. 단, 일치하는 문자 없을 경우 "존재하지 않음" 출력
+			System.out.println("존재하지 않음");
+		}
+		
+	}
 	
 }
